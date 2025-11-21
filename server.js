6a552d0e613e27a13e168861726d76a920c0bc53
Yousef Elsharkawy Abd El-fatah <121326305+yousefelsharkawy26@users.noreply.github.com>
2025-11-21 17:18:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app'); // Import the logic
 const PORT = 3000;
 
 // Connect to DB and then Start Server
-mongoose.connect('mongodb://localhost:27017/virtual-api')
+mongoose.connect('mongodb+srv://Vercel-Admin-virsual-api-db:aCsyAtABZquXeEuQ@virsual-api-db.htjvflj.mongodb.net/?retryWrites=true&w=majority')
     .then(() => {
         console.log('MongoDB Connected');
         app.listen(PORT, () => console.log(`Virtual System running on port ${PORT}`));
